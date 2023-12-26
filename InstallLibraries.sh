aba086669730c47ff72d91fac8ccc6c19efa2f49
blackflame576 <pws15548@gmail.com>
2023-12-26 18:30:00 +0300

    Add MACOSX_BUNDLE to CMakeLists.txt

--- InstallLibraries.sh
+++ InstallLibraries.sh
@@ -5,14 +5,19 @@ YUM_PACKAGE_NAME="make cmake gcc-c++ curl libcurl sqlite-devel openssl-devel"
 DEB_PACKAGE_NAME="g++ gcc build-essential cmake make curl libcurl4-openssl-dev libjsoncpp-dev libfmt-dev libsqlite3-dev libgtest-dev googletest google-mock libgmock-dev libtbb-dev libzip-dev zlib1g-dev"
 PACMAN_PACKAGE_NAME="jsoncpp gcc base-devel cmake  clang gtest lib32-curl libcurl-compat libcurl-gnutls curl fmt lib32-sqlite sqlite sqlite-tcl zlib"
 ZYPPER_PACKAGE_NAME="libcurl-devel gcc-c++ cmake gtest gmock zlib-devel fmt-devel sqlite3-devel jsoncpp-devel"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   # Mac OSX
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+   brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake
+fi
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
    if [ -f /etc/os-release ]; then
       # freedesktop.org and systemd
       . /etc/os-release
       OS=$NAME
-   elif type lsb_release >/dev/null 2>&1; then
-      # linuxbase.org
-      OS=$(lsb_release -si)
+   # elif type lsb_release >/dev/null 2>&1; then
+   #    # linuxbase.org
+   #    OS=$(lsb_release -si)
    elif [ -f /etc/lsb-release ]; then
       # For some versions of Debian/Ubuntu without lsb_release command
       . /etc/lsb-release
@@ -25,7 +30,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       ...
    elif [ -f /etc/redhat-release ]; then
       # Older Red Hat, CentOS, etc.
-      ...
+      OS="CentOS"
    else
       # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
       OS=$(uname -s)
@@ -106,10 +111,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       echo "Not found package manager"
       exit 1;
    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-   # Mac OSX
-   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-   brew  install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake
 fi
 echo "==> Libraries successfully installed"
 exit 0
