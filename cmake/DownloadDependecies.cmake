87f98950d3e1d7e3124acf1b3a1b2b0ad27427f5
blackflame576 <pws15548@gmail.com>
2024-05-11 14:31:12 +0300

    Refactoring

--- cmake/DownloadDependecies.cmake
+++ cmake/DownloadDependecies.cmake
@@ -3,11 +3,11 @@ FetchContent_Declare(fmt
   GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
   GIT_TAG master
 )
-if(NOT fmt_POPULATED)
-  FetchContent_Populate(fmt)
+# if(NOT fmt_POPULATED)
+#   FetchContent_Populate(fmt)
 
-  add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(fmt)
 
 # jsoncpp
@@ -15,11 +15,11 @@ FetchContent_Declare(jsoncpp
 GIT_REPOSITORY https://github.com/DeepForge-Technology/jsoncpp.git
 GIT_TAG master
 )
-if(NOT jsoncpp_POPULATED)
-  FetchContent_Populate(jsoncpp)
+# if(NOT jsoncpp_POPULATED)
+#   FetchContent_Populate(jsoncpp)
 
-  add_subdirectory(${jsoncpp_SOURCE_DIR} ${jsoncpp_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${jsoncpp_SOURCE_DIR} ${jsoncpp_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(jsoncpp)
 
 # SQLite
@@ -27,11 +27,11 @@ FetchContent_Declare(SQLite
   GIT_REPOSITORY https://github.com/DeepForge-Technology/SQLite.git
   GIT_TAG master
 )
-if(NOT SQLite_POPULATED)
-  FetchContent_Populate(SQLite)
+# if(NOT SQLite_POPULATED)
+#   FetchContent_Populate(SQLite)
 
-  add_subdirectory(${SQLite_SOURCE_DIR} ${SQLite_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${SQLite_SOURCE_DIR} ${SQLite_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(SQLite)
 
 # Logger
@@ -39,11 +39,11 @@ FetchContent_Declare(Logger
   GIT_REPOSITORY https://github.com/DeepForge-Tech/Logger.git
   GIT_TAG master
 )
-if(NOT Logger_POPULATED)
-  FetchContent_Populate(Logger)
+# if(NOT Logger_POPULATED)
+#   FetchContent_Populate(Logger)
 
-  add_subdirectory(${Logger_SOURCE_DIR} ${Logger_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${Logger_SOURCE_DIR} ${Logger_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(Logger)
 
 # libcurl
@@ -51,11 +51,11 @@ FetchContent_Declare(libcurl
   GIT_REPOSITORY https://github.com/DeepForge-Technology/curl.git
   GIT_TAG master
 )
-if(NOT libcurl_POPULATED)
-  FetchContent_Populate(libcurl)
+# if(NOT libcurl_POPULATED)
+#   FetchContent_Populate(libcurl)
 
-  add_subdirectory(${libcurl_SOURCE_DIR} ${libcurl_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${libcurl_SOURCE_DIR} ${libcurl_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(libcurl)
 
 # miniz
@@ -63,11 +63,11 @@ FetchContent_Declare(miniz
   GIT_REPOSITORY https://github.com/DeepForge-Tech/miniz.git
   GIT_TAG master
 )
-if(NOT miniz_POPULATED)
-  FetchContent_Populate(miniz)
+# if(NOT miniz_POPULATED)
+#   FetchContent_Populate(miniz)
 
-  add_subdirectory(${miniz_SOURCE_DIR} ${miniz_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${miniz_SOURCE_DIR} ${miniz_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(miniz)
 
 # Progressbar
@@ -75,11 +75,11 @@ FetchContent_Declare(Progressbar
   GIT_REPOSITORY https://github.com/DeepForge-Tech/Progressbar.git
   GIT_TAG master
 )
-if(NOT Progressbar_POPULATED)
-  FetchContent_Populate(Progressbar)
+# if(NOT Progressbar_POPULATED)
+#   FetchContent_Populate(Progressbar)
 
-  add_subdirectory(${Progressbar_SOURCE_DIR} ${Progressbar_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${Progressbar_SOURCE_DIR} ${Progressbar_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(Progressbar)
 
 # DatabaseAPI
@@ -87,9 +87,9 @@ FetchContent_Declare(DatabaseAPI
   GIT_REPOSITORY https://github.com/DeepForge-Tech/DatabaseAPI.git
   GIT_TAG master
 )
-if(NOT DatabaseAPI_POPULATED)
-  FetchContent_Populate(DatabaseAPI)
+# if(NOT DatabaseAPI_POPULATED)
+#   FetchContent_Populate(DatabaseAPI)
 
-  add_subdirectory(${DatabaseAPI_SOURCE_DIR} ${DatabaseAPI_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+#   add_subdirectory(${DatabaseAPI_SOURCE_DIR} ${DatabaseAPI_BINARY_DIR} EXCLUDE_FROM_ALL)
+# endif()
 FetchContent_MakeAvailable(DatabaseAPI)
